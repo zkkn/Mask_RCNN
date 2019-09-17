3d24e47386cb0b00cddde4a2d066f19be4022bb7
zkkn <shoichikanzaki@g.ecc.u-tokyo.ac.jp>
2019-09-17 05:46:51 +0000

    add function to save image of maskrccn

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -498,3 +498,67 @@ def display_weight_stats(model):
                 "{:+9.4f}".format(w.std()),
             ])
     display_table(table)
+
+def display_results(image, boxes, masks, class_ids, class_names, scores=None,
+                        show_mask=True, show_bbox=True, display_img=True,
+                        save_img=True, save_dir=None, img_name=None):
+        """
+        boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
+        masks: [height, width, num_instances]
+        class_ids: [num_instances]
+        class_names: list of class names of the dataset (Without Background)
+        scores: (optional) confidence scores for each box
+        show_mask, show_bbox: To show masks and bounding boxes or not
+        display_img: To display the image in popup
+        save_img: To save the predict image
+        save_dir: If save_img is True, the directory where you want to save the predict image
+        img_name: If save_img is True, the name of the predict image
+
+        """
+        n_instances = boxes.shape[0]
+        colors = color_map()
+        for k in range(n_instances):
+            color = colors[class_ids[k]].astype(np.int)
+            if show_bbox:
+                box = boxes[k]
+                cls = class_names[class_ids[k]-1]  # Skip the Background
+                score = scores[k]
+                cv2.rectangle(image, (box[1], box[0]), (box[3], box[2]), color, 1)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(image, '{}: {:.3f}'.format(cls, score), (box[1], box[0]),
+                            font, 0.4, (0, 255, 255), 1, cv2.LINE_AA)
+
+            if show_mask:
+                mask = masks[:, :, k]
+                color_mask = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.int)
+                color_mask[mask] = color
+                image = cv2.addWeighted(color_mask, 0.5, image.astype(np.int), 1, 0)
+
+        if display_img:
+            plt.imshow(image)
+            plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+            plt.show()
+        if save_img:
+            cv2.imwrite(os.path.join(save_dir, img_name), image)
+
+        return None
+
+def color_map(N=256, normalized=False):
+        def bitget(byteval, idx):
+            return ((byteval & (1 << idx)) != 0)
+
+        dtype = 'float32' if normalized else 'uint8'
+        cmap = np.zeros((N, 3), dtype=dtype)
+        for i in range(N):
+            r = g = b = 0
+            c = i
+            for j in range(8):
+                r = r | (bitget(c, 0) << 7 - j)
+                g = g | (bitget(c, 1) << 7 - j)
+                b = b | (bitget(c, 2) << 7 - j)
+                c = c >> 3
+
+            cmap[i] = np.array([r, g, b])
+
+        cmap = cmap / 255 if normalized else cmap
+        return cmap
